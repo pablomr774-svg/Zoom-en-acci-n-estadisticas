c9ddb642d219ad3c21bca55107900a1969b9cc3c
pablomr774-svg <pablomr774@gmail.com>
2026-08-31 15:19:34 -0600

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './style.css'
+export const metadata={title:'Zoom en Acción | Estadísticas'}
+export default function RootLayout({children}){return <html lang="es"><body>{children}</body></html>}
